e10ae2c0456b63de7505549ac67f56b435ed43f0
David Samuelson <linksn.1fan@gmail.com>
2022-03-17 14:59:28 -0600

    added 'when' statement

--- app.js
+++ app.js
@@ -23,9 +23,22 @@ const questions = [
         message: 'Enter your Github username?'
     },
     {
+        type: 'confirm',
+        name: 'confirmAbout',
+        message: 'Would you like to enter some information about yourself for an "About" section?',
+        default: true
+    },
+    {
         type: 'input',
         name: 'about',
-        message: 'Provide some information about yourself'
+        message: 'Provide some information about yourself',
+        when: ({ confirmAbout }) => {
+            if (confirmAbout) {
+                return true;
+            } else {
+                return false;
+            }
+        }
     }
 ];
 
