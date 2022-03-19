e2ac56036157c42a6ef8c9dcde967e7fe1f2d5d9
David Samuelson <linksn.1fan@gmail.com>
2022-03-19 10:16:29 -0600

    set up promises for app

--- app.js
+++ app.js
@@ -1,53 +1,7 @@
-const fs = require('fs');
 const inquirer = require('inquirer');
 
 const generatePage = require('./src/page-template.js');
-
-const mockData = {
-    name: 'David Samuelson',
-    github: 'dsamuelson',
-    confirmAbout: true,
-    about:
-        'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et.',
-    projects: [
-        {
-        name: 'Run Buddy',
-        description:
-            'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
-        languages: ['HTML', 'CSS'],
-        link: 'https://github.com/lernantino/run-buddy',
-        feature: true,
-        confirmAddProject: true
-        },
-        {
-        name: 'Taskinator',
-        description:
-            'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
-        languages: ['JavaScript', 'HTML', 'CSS'],
-        link: 'https://github.com/lernantino/taskinator',
-        feature: true,
-        confirmAddProject: true
-        },
-        {
-        name: 'Taskmaster Pro',
-        description:
-            'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
-        languages: ['JavaScript', 'jQuery', 'CSS', 'HTML', 'Bootstrap'],
-        link: 'https://github.com/lernantino/taskmaster-pro',
-        feature: false,
-        confirmAddProject: true
-        },
-        {
-        name: 'Robot Gladiators',
-        description:
-            'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque.',
-        languages: ['JavaScript'],
-        link: 'https://github.com/lernantino/robot-gladiators',
-        feature: false,
-        confirmAddProject: false
-        }
-    ]
-};
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 
 const questions = [
     {
@@ -80,42 +34,42 @@ const questions = [
     }
 ];
 
-// const promptUser = () => {
-//     let questions = [
-//         {
-//             type: 'input',
-//             name: 'name',
-//             message: 'What is your name?',
-//             validate: nameInput => {
-//                 if (nameInput) {
-//                     return true;
-//                 } else {
-//                     console.log('Please enter your name');
-//                     return false;
-//                 }
-//             }
-//         },
-//         {
-//             type: 'input',
-//             name: 'github',
-//             message: 'Enter your Github username?',
-//             validate: nameInput => {
-//                 if (nameInput) {
-//                     return true;
-//                 } else {
-//                     console.log('Please enter your name');
-//                     return false;
-//                 }
-//             }
-//         },
-//         {
-//             type: 'input',
-//             name: 'about',
-//             message: 'Provide some information about yourself'
-//         }
-//     ];
-//     return inquirer.prompt(questions)
-// };
+const promptUser = () => {
+    let questions = [
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your name');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your Github username?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your name');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'about',
+            message: 'Provide some information about yourself'
+        }
+    ];
+    return inquirer.prompt(questions)
+};
 
 const promptProject = portfolioData => {
     if (!portfolioData.projects) {
@@ -197,10 +151,22 @@ const promptProject = portfolioData => {
     });
 };
 
-// promptUser().then(promptProject).then((portfolioData) => {
-     const pageHTML = generatePage(mockData);
-    fs.writeFile('index.html', pageHTML, err => {
-    if (err) throw new Error(err);
+promptUser()
+    .then(promptProject)
+    .then((portfolioData) => {
+     return generatePage(portfolioData);
+    })
+    .then(pageHTML => {
+       return writeFile(pageHTML);
+    })
+    .then(writeFileResponse => {
+        console.log(writeFileResponse);
+        return copyFile();
+    })
+    .then(copyFileResponse => {
+        console.log(copyFileResponse);
+    })
+    .catch(err => {
+        console.log(err);
     });
-// });
-
+     
